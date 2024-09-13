9ee81433e28951b8ac7cd0d80df580a0c36b3948
Pash923 <75024830+Pash923@users.noreply.github.com>
2024-09-13 10:28:47 +0200

    Create dataPullAPI.py

---
+++ dataPullAPI.py
@@ -0,0 +1,11 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import urllib3
+import cx_Oracle
+from datetime import datetime
+import re
+import sys
+import os
+import time
+import logging
